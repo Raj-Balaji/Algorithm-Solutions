017deb39e6a46cd82d9932678b3b8b66330b7dc4
Raj-Balaji <raj.programmer27@gmail.com>
2022-06-11 15:19:01 -0400

    Add files via upload

---
+++ StairsProblem.java
@@ -0,0 +1,36 @@
+package stairspackage;
+
+import java.util.Scanner;
+
+public class StairsProblem {
+	
+	
+	public static void main(String args[])
+	{
+		
+		Scanner input = new Scanner(System.in);
+		int stairs,steps=0;
+		int f1,f2,f3;
+		f1=1;
+		f2=2;
+		f3 = f1+f2;
+		int a,b;
+		a = f3;
+		b = f2;
+		System.out.println("Enter the n, steps to calculate the different ways to climb");
+		stairs = Integer.parseInt(input.nextLine());
+		if(stairs== f1 || stairs == f2|| stairs==f3)
+		{
+			System.out.println("Steps required:"+stairs);
+		}
+		for(int i=4;i<=stairs;i++)
+		{
+			steps = a+ b;	// f(n) = f(n-1) + f(n-2)
+			b = a;			// f(n-2) -> move to f(n-1)
+			a = steps;		// f(n-1) -> move to f(n)
+		}
+		System.out.println("Different ways to climb :"+steps);
+		
+	}
+
+}
